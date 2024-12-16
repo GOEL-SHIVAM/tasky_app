b26ee904b0f8d8badd1a0c7d85ef4299b47ab51e
GOEL-SHIVAM <yourname@example.com131146872+GOEL-SHIVAM@users.noreply.github.com>
2024-12-17 00:04:37 +0530

    day4

---
+++ script.js
@@ -0,0 +1,9 @@
+const state = {
+  tasklist: [],
+};
+
+const taskContents = document.querySelector(".task__contents");
+
+const taskModal = document.querySelector(".task__modal__body");
+
+const htmlTaskContent = ({ id, title, description, type, url }) => {};
